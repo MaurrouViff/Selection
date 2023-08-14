f14f98e0c84ed02dfed42f9b0a917d7817869798
Aurélien <rofldu08@gmail.com>
2023-08-14 16:30:31 +0200

    Initial commit

---
+++ controleur/ControleurPrincipal.php
@@ -0,0 +1,24 @@
+<?php
+class ControleurPrincipal {
+    private $actions;
+
+    public function __construct() {
+        $this->actions = array(
+            "defaut" => "vueAccueil.php",
+            "accueil" => "vueAccueil.php"
+        );
+    }
+
+    public function execute($action) {
+        if (array_key_exists($action, $this->actions)) {
+            return $this->actions[$action];
+        } else {
+            return $this->actions["defaut"];
+        }
+    }
+}
+
+// Exemple d'utilisation
+$controller = new ControleurPrincipal();
+$action = "detail";
+$page = $controller->execute($action);
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "getRacine.php";
+include "$racine/controleur/ControleurPrincipal.php";
+include_once "$racine/modele/authentification.inc.php"; // pour pouvoir utiliser isLoggedOn()
+
+$controller = new ControleurPrincipal();
+
+if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+} else {
+    $action = "defaut";
+}
+
+$fichier = $controller->execute($action);
+include "$racine/controleur/$fichier";
