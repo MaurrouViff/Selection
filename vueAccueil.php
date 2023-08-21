b844fe52d7f7f8a64e7a25dfd3cd4bf0d8fc97b5
Aurélien <rofldu08@gmail.com>
2023-08-21 18:51:22 +0200

    Accueil

---
+++ vueAccueil.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $titre ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="">
+</head>
+<body>
+<div class="container">
+    <header>
+        <div class="gradient-policy"><h1>Bienvenue sur Projet Sélection</h1></div>
+
+        <nav class="navbar">
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="./?action=propos" class="blue-btn">Se connecter</a>
+                </li>
+
+            </ul>
+        </nav>
+        <hr class="hr-gradient">
+    </header>
+
+
+</div>
+</body>
+</html>
