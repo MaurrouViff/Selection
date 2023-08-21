7d085f8d006e59973b7c024ca03bfd21e04fe1a9
Aurélien <rofldu08@gmail.com>
2023-08-21 18:51:44 +0200

    Page de connexion

---
+++ vueConnexion.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $titre ?>></title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="">
+</head>
+<body>
+<header>
+
+</header>
+<div class="container">
+</div>
+<?php include "WebsitePart/footer/footer.php"; ?>
+</body>
+</html>
