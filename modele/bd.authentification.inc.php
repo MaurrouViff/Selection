3ef7b12797e6e32a4e107888a207ba3471d56007
Aurélien <rofldu08@gmail.com>
2023-08-30 17:36:18 +0200

    modele de l'auth

--- modele/bd.authentification.inc.php
+++ modele/bd.authentification.inc.php
@@ -1,59 +1,57 @@
 <?php
-include_once ("bd.user.inc.php");
-$utilisateur = new Utilisateur();
-class Authentification {
-    private $mailU;
-    private $mdpU;
 
-    public function __construct($mailU, $mdpU) {
-        $this->mailU=$mailU;
-        $this->mdpU=$mdpU;
+include_once "bd.user.inc.php";
+
+function login($mailU, $mdpU) {
+    if (!isset($_SESSION)) {
+        session_start();
     }
-     public function login() {
-        global $utilisateur;
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-        $util = $utilisateur->getUtilisateurByMailU($this->mailU);
-        if ($util) {
-            $mdpBD = $util["mdpU"];
-        } else {
-            $mdpBD = "false";
-        }
-        if (trim($mdpBD) == trim(crypt($this->mdpU, $mdpBD))) {
-            $_SESSION["mailU"] = $this->mailU;
-            $_SESSION["mdpU"] = $mdpBD;
-        }
+
+    $util = getUtilisateurByMailU($mailU);
+    if ($util){
+        $mdpBD = $util["mdpU"];
+    }
+    else {$mdpBD="faux";}
+
+    if (trim($mdpBD) == trim(crypt($mdpU, $mdpBD))) {
+        // le mot de passe est celui de l'utilisateur dans la base de donnees
+        $_SESSION["mailU"] = $mailU;
+        $_SESSION["mdpU"] = $mdpBD;
     }
 }
-     function logout() {
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-        unset($_SESSION["mailU"]);
-        unset($_SESSION["mdpU"]);
-    }
-     function isLoggedOn() {
-        global $utilisateur;
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-        $ret = false;
 
-        if (isset($_SESSION["mailU"])) {
-            $util = $utilisateur->getUtilisateurByMailU(($_SESSION["mailU"]));
-            if ($util["mailU"] == $_SESSION["mailU"] && $util["mdpU"] == $_SESSION["mdpU"]) {
-                $ret = true;
-            }
-     }
-        return $ret;
+function logout() {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    unset($_SESSION["mailU"]);
+    unset($_SESSION["mdpU"]);
 }
-    function getMailUloggedOn() {
-    if (isLoggedOn()) {
+
+function getMailULoggedOn(){
+    if (isLoggedOn()){
         $ret = $_SESSION["mailU"];
-    } else {
+    }
+    else {
         $ret = "";
-
     }
     return $ret;
+
+}
+
+function isLoggedOn()
+{
+    if (!isset($_SESSION)) {
+        session_start();
     }
+    $ret = false;
+
+    if (isset($_SESSION["mailU"])) {
+        $util = getUtilisateurByMailU($_SESSION["mailU"]);
+        if ($util["mailU"] == $_SESSION["mailU"] && $util["mdpU"] == $_SESSION["mdpU"]
+        ) {
+            $ret = true;
+        }
+    }
+    return $ret;
+}
