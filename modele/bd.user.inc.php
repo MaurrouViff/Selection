6ecddf55d3ad5e0f18af01a5b147fcd457756c93
Aurélien <rofldu08@gmail.com>
2023-08-30 17:38:44 +0200

    modele pour les utilisateurs

--- modele/bd.user.inc.php
+++ modele/bd.user.inc.php
@@ -1,46 +1,53 @@
 <?php
 
 include_once ("bd.inc.php");
-class Utilisateur extends Database {
-    public function getUtilisateurs() {
-        $result = array();
-        try {
-            $req = $this->connexion->prepare("select * from user");
-            $req->execute();
-            $result = $req->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage();
-            die();
-        }
-        return $result;
+function getUtilisateurs() {
+    $resultat = array();
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from user");
+        $req->execute();
+
+        $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
     }
-    public function addUtilisateur($mailU, $mdpU, $pseudoU) {
-        $result = array();
-        try {
-            $mdpUCrypt = crypt($mdpU, "sel");
-            $req = $this->connexion->prepare("insert into user (mailU, mdpU, pseudoU, rankU, libelleC) values (:mailU, :mdpU, :pseudoU, 'eleve', 'eleve par défaut')");
-            $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
-            $req->bindValue(':mdpU', $mdpUCrypt, PDO::PARAM_STR);
-            $req->bindValue(':pseudoU', $pseudoU, PDO::PARAM_STR);
-            $req->execute();
-            $result = $req->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage();
-            die();
-        }
-        return $result;
+    return $resultat;
+}
+
+function getUtilisateurByMailU($mailU) {
+    $resultat = array();
+    try {
+
+        $cnx = connexionPDO();
+
+        $req = $cnx->prepare("select * from user where mailU=:mailU");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->execute();
+
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
     }
-    public function getUtilisateurByMailU($mailU) {
-        $result = array();
-        try {
-            $req = $this->connexion->prepare("select * from user where mailU=:mailU");
-            $req->bindValue(':amilU', $mailU, PDO::PARAM_STR);
-            $req->execute();
-            $result = $req->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage();
-            die();
-        }
-        return $result;
+    return $resultat;
+}
+
+function addUtilisateur($mailU, $mdpU, $pseudoU) {
+    try {
+        $cnx = connexionPDO();
+        $mdpUCrypt = crypt($mdpU, "sel");
+        $req = $cnx->prepare("insert into user (mailU, passwordU, pseudoU, rankU, libelleC) values (:mailU, :passwordU, :pseudoU, 'eleve', 'eleve par défaut')");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->bindValue(':passwordU', $mdpUCrypt, PDO::PARAM_STR);
+        $req->bindValue(':pseudoU', $pseudoU, PDO::PARAM_STR);
+
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
     }
+    return $resultat;
 }
