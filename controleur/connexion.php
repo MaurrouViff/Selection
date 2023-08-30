4ef3111cd2f8f2bed7ab4710c74ef6ae59c6bcc6
Aurélien <rofldu08@gmail.com>
2023-08-30 17:38:25 +0200

    controleur pour la connexion

--- controleur/connexion.php
+++ controleur/connexion.php
@@ -2,21 +2,24 @@
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
-
+include_once ("$racine/modele/bd.authentification.inc.php");
 
 // recuperation des donnees GET, POST, et SESSION
 if (isset($_POST["mailU"]) && isset($_POST["mdpU"])){
     $mailU = $_POST["mailU"];
     $mdpU = $_POST["mdpU"];
-}
-else
-{
+} else {
     $mailU = "";
     $mdpU = "";
 }
 
+login($mailU,$mdpU);
 
 
+if (isLoggedOn()) {
+    include "$racine/controleur/monProfil.php";
+} else {
+    $titre = "Connexion - Selection";
+    include ("$racine/vueConnexion.php");
+}
 
-$titre = "Connexion - Selection";
-include ("$racine/vueConnexion.php");
