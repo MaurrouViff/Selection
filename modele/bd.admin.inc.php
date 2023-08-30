cebfc0533658a4128329a0b8fd43882842935c84
Aurélien <rofldu08@gmail.com>
2023-08-30 17:40:38 +0200

    modele pour les admins (à faire)

--- modele/bd.admin.inc.php
+++ modele/bd.admin.inc.php
@@ -1,18 +1,3 @@
 <?php
 
 include_once ("bd.inc.php");
-class Admin extends Database {
-    public function getAdmin() {
-        $result = array();
-        try {
-            $req = $this->connexion->prepare("select * from user WHERE rankU = 'admin'");
-            $req->execute();
-            $result = $req->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage();
-            die();
-        }
-        return $result;
-    }
-
-}
