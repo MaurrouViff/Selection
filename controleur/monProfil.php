04a229a931d88f7b8af981dff9794d59cd3aa774
Aurélien <rofldu08@gmail.com>
2023-08-30 17:37:53 +0200

    controleur pour son profil

---
+++ controleur/monProfil.php
@@ -0,0 +1,11 @@
+<?php
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
+    $racine="..";
+}
+include_once "$racine/modele/authentification.inc.php";
+include_once "$racine/modele/bd.utilisateur.inc.php";
+
+
+
+$titre = "Inscription - Selection";
+include ("$racine/vueAccueil.php");
